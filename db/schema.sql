745ab05989f1502d5c4e2551ece70e9e6fd288c0
Joseph Young <joseph.r.young1@gmail.com>
2019-11-16 22:42:29 -0500

    Update database with santa assignments

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,18 @@
 CREATE TABLE people
 (
-	person_ID 		INTEGER PRIMARY KEY AUTOINCREMENT,
-	email			VARCHAR NOT NULL UNIQUE
+	person_ID 			INTEGER PRIMARY KEY AUTOINCREMENT,
+	email				VARCHAR NOT NULL UNIQUE,
+	givingTo			VARCHAR,
+	givingTo_ID			INTEGER
 );
 
 CREATE TABLE forbiddenPairings
 (
-	constraint_ID 	INTEGER PRIMARY KEY AUTOINCREMENT,
-	santa			VARCHAR NOT NULL,
-	santa_ID		INTEGER NOT NULL,
-	assignment		VARCHAR NOT NULL,
-	assignment_ID	INTEGER NOT NULL,
+	constraint_ID 		INTEGER PRIMARY KEY AUTOINCREMENT,
+	santa				VARCHAR NOT NULL,
+	santa_ID			INTEGER NOT NULL,
+	assignment			VARCHAR NOT NULL,
+	assignment_ID		INTEGER NOT NULL,
 	FOREIGN KEY(santa_ID) REFERENCES people(person_ID)
 	FOREIGN KEY(assignment_ID) REFERENCES people(person_ID)
 );
