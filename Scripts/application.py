d23445f3d7fa560041cac4341c04fd7419f9fa3c
Joseph Young <joseph.r.young1@gmail.com>
2020-01-03 14:42:48 -0500

    Made API more RESTful

--- Scripts/application.py
+++ Scripts/application.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 app = Flask(__name__)
 
 path_to_db = '../db/allYourSantaAreBelongToUs.db'
+BAD_REQUEST_ERROR = 400
 
 @app.route('/')
 def hello_world():
@@ -14,42 +15,41 @@ def hello_world():
     
 @app.route('/api/v1/person')
 def get_all_people():
-    
-    if request.method == "GET":
-        # return a json of all people
-        with sqlite3.connect(path_to_db) as connection:
-            cursor = connection.cursor()
-            #get all of the people
-            cursor.execute(
-                'SELECT person_ID, email, givingTo_ID, first_name, last_name '
-                'FROM people')
-            raw_data: List = cursor.fetchall()
-            data: List = []
-            for entry in raw_data:
-                data.append(
-                    {"person_ID": entry[0],
-                    "email": entry[1],
-                    "assignment ID": entry[2],
-                    "first_name": entry[3],
-                    "last_name": entry[4]})
-            json_data = json.dumps(data)
-        return json_data
+       
+    # return a json of all people
+    with sqlite3.connect(path_to_db) as connection:
+        cursor = connection.cursor()
+        #get all of the people
+        cursor.execute(
+            'SELECT person_ID, email, givingTo_ID, first_name, last_name '
+            'FROM people')
+        raw_data: List = cursor.fetchall()
+        data: List = []
+        for entry in raw_data:
+            data.append(
+                {"person_ID": entry[0],
+                "email": entry[1],
+                "assignment ID": entry[2],
+                "first_name": entry[3],
+                "last_name": entry[4]})
+        json_data = json.dumps(data)
+    return json_data
         
 @app.route('/api/v1/person', methods = ["POST"])
-def person_request():
+def post_person():
     
     # verify that request contains a valid request  
     try:
         request_json: Dict = request.get_json()
     except:
         abort(
-            400 ,
+            BAD_REQUEST_ERROR ,
             description = "Please verify that request contains valid json.")
     try:
         posted_email = request_json['email']
     except:
         abort(
-            400,
+            BAD_REQUEST_ERROR,
             description = "Please verify that  request contains an email.")
     
     # add person to database         
@@ -81,43 +81,45 @@ def person_request():
             "last_name": posted_record[4]}
         return json.dumps(data)
  
-@app.route('/api/v1/person/<person_id>', methods = ["GET","DELETE"])
-def get_person(person_id: str) -> str:
-    if request.method == "DELETE":
-        with sqlite3.connect(path_to_db) as connection: 
-            cursor = connection.cursor()
-            try:
-                cursor.execute(
-                    'DELETE FROM people WHERE person_ID=?;',
-                    (person_id,))
-                command = 'DELETE FROM forbiddenPairings WHERE santa_ID=? or ' \
-                          'assignment_ID=?;'
-                cursor.execute(command, (person_id, person_id))
-            except:
-                return(
+@app.route('/api/v1/person/<person_id>')
+def get_person(person_id: str) -> str:   
+    
+    # returns a json of <person_id>
+    with sqlite3.connect(path_to_db) as connection: 
+        cursor = connection.cursor()
+        try:
+            command = 'SELECT person_ID, email, givingTo_ID FROM people ' \
+                      'WHERE person_ID=?;'
+            cursor.execute(command, (person_id,))
+            raw_data: Tuple = cursor.fetchone()
+            data: List = [
+                {
+                    "person_ID": raw_data[0],
+                    "email": raw_data[1],
+                    "assignment ID": raw_data[2]}]
+            json_data = json.dumps(data)
+        except:
+            return(
                 'There was an error. Check that the requested ID is valid.')
-        return "user has been removed from database"
+    return json_data
     
-    if request.method == "GET":
-        # returns a json of <person_id>
-        with sqlite3.connect(path_to_db) as connection: 
-            cursor = connection.cursor()
-            try:
-                command = 'SELECT person_ID, email, givingTo_ID FROM people ' \
-                          'WHERE person_ID=?;'
-                cursor.execute(command, (person_id,))
-                raw_data: Tuple = cursor.fetchone()
-                data: List = [
-                    {
-                        "person_ID": raw_data[0],
-                        "email": raw_data[1],
-                        "assignment ID": raw_data[2]}]
-                json_data = json.dumps(data)
-            except:
-                return(
-                    'There was an error. Check that the requested ID is valid.')
-        return json_data
+@app.route('/api/v1/person/<person_id>', methods = ["DELETE"])
+def delete_person(person_id: str) -> str:
     
+    with sqlite3.connect(path_to_db) as connection: 
+        cursor = connection.cursor()
+        try:
+            cursor.execute(
+                'DELETE FROM people WHERE person_ID=?;',
+                (person_id,))
+            command = 'DELETE FROM forbiddenPairings WHERE santa_ID=? or ' \
+                      'assignment_ID=?;'
+            cursor.execute(command, (person_id, person_id))
+        except:
+            return(
+            'There was an error. Check that the requested ID is valid.')
+    return "user has been removed from database"
+
 @app.route('/api/v1/forbidden_pairing')
 def get_forbidden_pairings():
     
@@ -140,27 +142,27 @@ def get_forbidden_pairings():
     return json_data
         
 @app.route('/api/v1/forbidden_pairing', methods = ["POST"])
-def forbidden_pairing_request():
+def post_forbidden_pairing():
     
     # verify that request is valid
     try:
         request_json = request.get_json()
     except:
         abort(
-            400,
+            BAD_REQUEST_ERROR,
             description = "Please verify that request contains valid json.")
     try:
         new_giver_constraint = request_json['santa_ID']
     except:
         abort(
-            400,
+            BAD_REQUEST_ERROR,
             description = "Please verify that request contains a "
                           "'santa_ID' key.")
     try:
         new_receiver_constraint = request_json['assignment_ID']
     except:
         abort(
-            400,
+            BAD_REQUEST_ERROR,
             description = "Please verify that request contains an "
                           "'assignment_ID' key.")
     
@@ -195,43 +197,45 @@ def forbidden_pairing_request():
             "assignment_ID": posted_record[4]}
         return json.dumps(data)
 
-@app.route('/api/v1/forbidden_pairing/<id>', methods = ['GET', 'DELETE'])
-def get_pairing(id: str) -> str:
-    if request.method == 'DELETE':
-        with sqlite3.connect(path_to_db) as connection: 
-            cursor = connection.cursor()
-            try:
-                cursor.execute(
-                    'DELETE FROM forbiddenPairings WHERE constraint_ID=?;',
-                    (id,))
-            except:
-                return('There was an error. Check that the requested ID is '
-                       'valid.')
-        return "constraint has been deleted"
-    
+@app.route('/api/v1/forbidden_pairing/<id>')
+def get_pairing(id: str) -> str:   
+ 
     # returns a json of specified ID
-    if request.method == 'GET':
-        with sqlite3.connect(path_to_db) as connection:
-            cursor = connection.cursor()
-            try:
-                command = ('SELECT constraint_ID, santa, santa_ID, assignment, '
-                           'assignment_ID FROM forbiddenPairings WHERE '
-                           'constraint_ID=?;')
-                cursor.execute(command, (id,))
-                raw_data: Tuple = cursor.fetchone()
-                data: List = [
-                    {
-                        "constraint_ID": raw_data[0],
-                        "santa": raw_data[1],
-                        "santa_ID": raw_data[2],
-                        "assignment": raw_data[3],
-                        "assignment_ID": raw_data[4]}]
-                json_data = json.dumps(data)
-            except:
-                return('There was an error. Check that the requested ID is '
-                       'valid.')
-        return json_data
+    with sqlite3.connect(path_to_db) as connection:
+        cursor = connection.cursor()
+        try:
+            command = ('SELECT constraint_ID, santa, santa_ID, assignment, '
+                       'assignment_ID FROM forbiddenPairings WHERE '
+                       'constraint_ID=?;')
+            cursor.execute(command, (id,))
+            raw_data: Tuple = cursor.fetchone()
+            data: List = [
+                {
+                    "constraint_ID": raw_data[0],
+                    "santa": raw_data[1],
+                    "santa_ID": raw_data[2],
+                    "assignment": raw_data[3],
+                    "assignment_ID": raw_data[4]}]
+            json_data = json.dumps(data)
+        except:
+            return('There was an error. Check that the requested ID is '
+                   'valid.')
+    return json_data
         
-@app.errorhandler(400)
+@app.route('/api/v1/forbidden_pairing/<id>', methods = ['DELETE'])
+def delete_pairing(id: str) -> str:
+
+    with sqlite3.connect(path_to_db) as connection: 
+        cursor = connection.cursor()
+        try:
+            cursor.execute(
+                'DELETE FROM forbiddenPairings WHERE constraint_ID=?;',
+                (id,))
+        except:
+            return('There was an error. Check that the requested ID is '
+                   'valid.')
+    return "constraint has been deleted"
+
+@app.errorhandler(BAD_REQUEST_ERROR)
 def resource_not_found(err):
-    return jsonify(error=str(err)), 400
+    return jsonify(error=str(err)), BAD_REQUEST_ERROR
